2b42e96a572db02dbf4fc0175811e5154d04d03b
TailaiYing <taiying9627@gmail.com>
2025-06-28 11:25:53 -0600

    position logging at every time step

--- main.py
+++ main.py
@@ -33,34 +33,28 @@ time_steps = 0
 for i in range(0, 2):
     drones.append(Drone())
 
-for _ in range(1000):
-    for drone in drones:
-        p.changeDynamics(drone.drone_id, -1, linearDamping=0.0, angularDamping=0.0)
-        other_drones = [other for other in drones if other != drone]
-        sensor_input = []
-        for sensor in drone.sensors:
-            sensor_input.append(sensor.detect(other_drones))
-        capability_model = drone.controller.capability_model(sensor_input)
-        drone.controller.apply_capability_model(capability_model)
-
-
-    p.stepSimulation()
-    time.sleep(1./240.)
-    time_steps += 1
-
-for drone in drones:
-    positions[drone.drone_id]= drone.position
-    
-print("\n")
-print("\npositions of drones after", time_steps, "time steps")
-pprint.pprint(positions)  # Pretty print to terminal
-
-# Save to CSV
+# open csv before loop
 with open("drone_positions.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Controller model: " + " ".join(str(x) for x in drones[0].controller.model)])
-    writer.writerow(["drone_id", "x", "y", "z"])
-    for drone_id, pos in positions.items():
-        writer.writerow([drone_id] + list(pos))
+    writer.writerow(["time_step", "drone_id", "x", "y", "z"])
+
+    for step in range(100):
+        for drone in drones:
+            p.changeDynamics(drone.drone_id, -1, linearDamping=0.0, angularDamping=0.0)
+            other_drones = [other for other in drones if other != drone]
+            sensor_input = []
+            for sensor in drone.sensors:
+                sensor_input.append(sensor.detect(other_drones))
+            capability_model = drone.controller.capability_model(sensor_input)
+            drone.controller.apply_capability_model(capability_model)
+
+        p.stepSimulation()
+        time.sleep(1./240.)
+
+        # write positions for each drone at this time step
+        for drone in drones:
+            pos = drone.position
+            writer.writerow([step, drone.drone_id] + list(pos))
 
 print("Positions saved to drone_positions.csv\n")
